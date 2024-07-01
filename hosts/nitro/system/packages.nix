b96a152d497a157b7d1775ba014070c11f64dd53
Jakku Night <R101g117l117s@gmail.com>
2024-07-01 13:22:02 -0400

    Add new dev runtimes/compilers.

--- hosts/nitro/system/packages.nix
+++ hosts/nitro/system/packages.nix
@@ -7,27 +7,42 @@
   nixpkgs.config.allowUnfree = true;
   
   environment.systemPackages = with pkgs; [
-    git
-    #neovim
-    micro
-    nix-ld
-    yazi
-    zellij
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    btop
-    htop
-    neofetch
-    waybar
-    wofi
-    alacritty
-    swww
-    dbus
-    gvfs
-    udisks2
-    udiskie
-    usbutils
+    git # Version Control system.
+    neovim # Main text editor
+    micro # Main text editor (for simple edits)
+    nix-ld # Linker for NixOS
+    yazi # Main filemanager (for the terminal)
+    zellij # Main terminal multiplexer
+    tmux # Secondary terminal multiplexer
+    vim # Base text editor
+    wget # Utility to download files from the web
+    btop # A great tool for monitoring processes
+    htop # The classic HTOP to monitor processes
+    neofetch # A CLI system info display
+    waybar # The bar for Wayland compositors (Hyprland in this case)
+    wofi # The app launcher for Wayland compositors (Hyprland in this case)
+    alacritty # My Main GPU powered terminal emulator
+    swww # A program to set the wallpaper on Wayland
+    dbus # The most popular intercommunication process tool.
+    gvfs # A service to autodetect USB drives.
+    udisks2 # A tool to manage USB devices.
+    udiskie # A tool for automounting USB drives.
+    usbutils # Utilities for USB mangement.
+    # To setup IPSec connections:
     strongswan
     networkmanager_strongswan
+    # Development tools:
+    python
+    nodejs
+    rustup
+    go
+    php
+    gcc
+    cmake
+    ninja
+    bison
+    meson
+    premake
+    lua
   ];
 }
