8b76166e81f2890377f5fe31b6e3ed90bd80d10a
Jakku Night <R101g117l117s@gmail.com>
2024-07-05 08:37:11 -0400

    Basic network config added.

--- hosts/nitro/system/networking.nix
+++ hosts/nitro/system/networking.nix
@@ -1,21 +1,8 @@
 # Here goes the networking configuration:
 { config, lib, pkgs, ... }@inputs: {
-  #networking.hostName = "nitro"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  #networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+.
   # Set your time zone.
   time.timeZone = "America/Asuncion";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Network configuration:
   networking = {
