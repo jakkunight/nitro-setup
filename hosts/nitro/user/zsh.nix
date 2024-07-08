581931c1bf4b1ef233eff7d1688e8ae6c1cd72ec
Jakku Night <R101g117l117s@gmail.com>
2024-07-08 14:31:08 -0400

    Add ZSH + Powerlevel10k config.

--- hosts/nitro/user/zsh.nix
+++ hosts/nitro/user/zsh.nix
@@ -12,13 +12,8 @@
     plugins = [
       {
         name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "~/.share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource./p10k-config;
-        file = "~/p10k.zsh";
+        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+        file = "powerlevel10k.zsh-theme";
       }
     ];
   };
