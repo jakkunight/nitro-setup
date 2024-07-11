c18f824c901dd70d45394acb4cee83b4c6058df9
Jakku Night <R101g117l117s@gmail.com>
2024-07-11 11:16:13 -0400

    Add Hyprland config to home-manager.

--- hosts/nitro/user/hyprland.nix
+++ hosts/nitro/user/hyprland.nix
@@ -22,6 +22,18 @@
         layout = "dwindle";
       };
       # Keybindings
+      bindel = [
+        # Multimedia:
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
+      bindl = [
+        # Multimedia:
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86AudioNext, exec, playerctl next"
+      ];
       bindr = [ ];
       bind = [
         # Open a terminal:
