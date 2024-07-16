e42c0691d7ad6f7d47d4d02f4396266e717138e1
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 08:27:59 -0400

    Add common networking config.

---
+++ networking/common.nix
@@ -0,0 +1,11 @@
+# Configure Hostname and timezone. Enable networking.
+{}:
+{
+  # Set your time zone.
+  time.timeZone = "America/Asuncion";
+  stName = "nitro";
+  # Uses NetworkManager, as this is easier to use
+  networkmanager = {
+    enable = true;
+  };
+}
