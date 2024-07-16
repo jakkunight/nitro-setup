88aecb8f08476023ae078ca49da63eeecb14ba4e
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 08:22:05 -0400

    Add network interfaces.

---
+++ networking/interfaces.nix
@@ -0,0 +1,11 @@
+# Network interfaces setup:
+{}:
+{
+  # Setup the network interfaces, such Wireless, Ethernet, etc:
+  interfaces = {
+    # Wireless interface:
+    wlp62s0 = {
+      useDHCP = true;
+    };
+  };
+}
