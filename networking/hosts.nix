8eb121548f0cd1a52e76f7c5c2a0503037794555
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 08:20:01 -0400

    Add hosts to network config.

---
+++ networking/hosts.nix
@@ -0,0 +1,19 @@
+# Hosts:
+{}:
+{
+  networking.hosts = {
+    "142.251.133.4" = [
+      "www.google.com"
+      "google.com"
+    ];
+    "185.178.208.140" = [
+      "m.animeflv.net"
+      "animeflv.net"
+    ];
+    "142.251.134.14" = [
+      "youtube.com"
+      "m.youtube.com"
+      "www.youtube.com"
+    ];
+  };
+}
