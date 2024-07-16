9c150695058570c434daff059ba892af01ba5a4c
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 10:17:26 -0400

    Add systemwide Hyprland config.

---
+++ graphics/hyprland.nix
@@ -0,0 +1,11 @@
+# Hyprland systemwide config:
+{ pkgs }:
+{
+  # Enable Hyprland:
+  programs.hyprland.enable = true;
+
+  # Install dunst:
+  environment.systemPackages = with pkgs; [
+    dunst
+  ];
+}
