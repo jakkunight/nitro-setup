30ffa83d632faf047f6b2bebeb3ad24e464a1761
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 10:38:26 -0400

    Add Neovim config to home-manager.

---
+++ users/jakku/apps/nvim.nix
@@ -0,0 +1,21 @@
+# Neovim config:
+{ pkgs }:
+{
+  programs.neovim = {
+    enable = true;
+    extraPackages = with pkgs; [
+      # LazyVim
+      vimPlugins.LazyVim
+      lua-language-server
+      stylua
+      # Telescope
+      ripgrep
+      fd
+      lazygit
+      luajitPackages.luarocks
+    ];
+    plugins = with pkgs.vimPlugins; [
+      LazyVim
+    ];
+  };
+}
