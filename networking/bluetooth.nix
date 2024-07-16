51fe096c915e87dcbfde0045361670f59bb88edf
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 09:03:00 -0400

    Add bluetooth config.

---
+++ networking/bluetooth.nix
@@ -0,0 +1,10 @@
+# Bluetooth config (even if it does not work):
+{}:
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  # Enable Blueman:
+  services.blueman.enable = true;
+}
