f2194c624668eaf43ca0f95d8c934affc6ff50a2
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 17:23:19 +0000

    Add missing dependencies.

--- graphics/sddm.nix
+++ graphics/sddm.nix
@@ -1,6 +1,12 @@
 # SDDM config:
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    # Qt5:
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+  ];
+
   services.displayManager.sddm = {
     enable = true;
     theme = "${import ./../assets/sddm-theme.nix { inherit pkgs; }}";
