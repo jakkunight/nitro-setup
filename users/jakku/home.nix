5eac04549c5abb9d0d07d5f28b83565aa51b106d
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 10:30:02 -0400

    Add initial home.nix file.

---
+++ users/jakku/home.nix
@@ -0,0 +1,31 @@
+# Home-Manager config:
+{}: {
+  # Initial info:
+  home.username = "jakku";
+  home.homeDirectory = "/home/jakku";
+
+  # DO NOT EDIT THIS!!!
+  home.stateVersion = "24.05";
+
+  # Session variables:
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    EDITOR = "nvim";
+    VISUAL = "micro";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Import the configs:
+  imports = [
+    ./apps/git.nix
+    ./apps/waybar.nix
+    ./apps/hyprland.nix
+    ./apps/zsh.nix
+    ./apps/qt.nix
+    ./apps/gtk.nix
+    ./apps/nvim.nix
+  ];
+
+}
