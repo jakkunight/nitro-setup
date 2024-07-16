af85fa6f2d8c09e8ea48dd6c7921061db5c1be83
Jakku Night <R101g117l117s@gmail.com>
2024-07-15 22:29:01 -0400

    Add GRUB2 config.

---
+++ bootloader/grub2.nix
@@ -0,0 +1,15 @@
+# A file to mannage my GRUB2 configuration.
+# [Coming Soon] A GRUB theme.
+{}:
+{
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "nidev";
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+      };
+    };
+  };
+}
