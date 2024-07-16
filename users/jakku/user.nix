0cfe2c163f033581bc6b38703fc1e73dc9e0c4eb
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 10:24:49 -0400

    Add initial user config.

---
+++ users/jakku/user.nix
@@ -0,0 +1,28 @@
+# Users config:
+{ pkgs }:
+{
+  # Use ZSH as default shell:
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  users.users.jakku.useDefaultShell = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jakku = {
+    isNormalUser = true;
+    initialPassword = "1234";
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+
+    # User packages:
+    packages = with pkgs; [
+      firefox
+      libreoffice
+      brave
+      discord
+      remmina
+      audacity
+      kdePackages.kdenlive
+      obs-studio
+    ];
+  };
+
+}
