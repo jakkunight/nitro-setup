bdbac7a1515481fb460f7d1b389a1b0eb44dfa61
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 09:11:46 -0400

    Enable USB automount. Rename service folders.

---
+++ services/usb.nix
@@ -0,0 +1,16 @@
+# USB Services:
+{}:
+{
+  # Enable GVFS to mount USB drives:
+  services.gvfs.enable = true;
+
+  # Enable udisks2:
+  services.udisks2.enable = true;
+
+  # Enable udiskie:
+  services.udiskie = {
+    enable = true;
+    notify = true;
+    try = true;
+  };
+}
