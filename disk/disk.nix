eda5aa618fc9259a44e172bc3dccc836550493c9
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 07:28:32 -0400

    Add disko config. 1G BOOT + 16G SWAP + ...ROOT.

---
+++ disk/disk.nix
@@ -0,0 +1,42 @@
+{ disks ? [ "/dev/sda" ] }:
+{
+  disko.devices = {
+    disk = {
+      nitro = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            swap = {
+              size = "16G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                priority = 100;
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
