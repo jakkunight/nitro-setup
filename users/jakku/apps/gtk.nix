95b3e7f7644926afcdbb486fdf58b9c765f61f08
Jakku Night <R101g117l117s@gmail.com>
2024-07-16 10:34:17 -0400

    Add GTK themes.

---
+++ users/jakku/apps/gtk.nix
@@ -0,0 +1,32 @@
+# GTK config:
+{ pkgs }:
+{
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.breeze-hacked-cursor-theme;
+    name = "Breeze_Hacked"; # See https://store.kde.org/p/1440328 to find the selector name.
+    size = 36;
+  };
+  gtk = {
+    enable = true;
+    font = {
+      name = "Cascadia Code";
+      package = pkgs.cascadia-code;
+      size = 12;
+    };
+    theme = {
+      name = "Tokyonight-Dark-B";
+      package = pkgs.tokyonight-gtk-theme;
+    };
+    iconTheme = {
+      package = pkgs.candy-icons;
+      name = "candy-icons";
+    };
+    cursorTheme = {
+      package = pkgs.breeze-hacked-cursor-theme;
+      name = "Breeze_Hacked"; # See https://store.kde.org/p/1440328 to find the selector name.
+      size = 36;
+    };
+  };
+}
