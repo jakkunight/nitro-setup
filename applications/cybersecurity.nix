3b4df6aca7e575dde2f32056d942d7969ff153a8
Jakku Night <R101g117l117s@gmail.com>
2024-07-23 16:49:34 -0400

    Add more cybersecurity tools.

--- applications/cybersecurity.nix
+++ applications/cybersecurity.nix
@@ -1,4 +1,21 @@
 # Cybersecurity tools.
 # Use them wisely.
-{ ... }:
-{ }
+{ pkgs, config, lib, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    wireshark
+    nmap
+    hydra-cli
+    thc-hydra
+    hashcat
+    hashcat-utils
+    hcxtools
+    macchanger
+    aircrack-ng
+    john
+    crackmapexec
+    responder
+  ];
+
+
+}
