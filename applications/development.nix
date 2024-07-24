2c2354168fa59b3895d1ca7aabe8c5c6a796e800
Jakku Night <R101g117l117s@gmail.com>
2024-07-24 08:42:23 -0400

    Remove unnecessary tools.

--- applications/development.nix
+++ applications/development.nix
@@ -7,27 +7,5 @@
     cachix
     devenv
     devbox
-    rustup
-    cargo
-    bun
-    nodejs
-    cmake
-    gnumake
-    gcc
-    stdenv
-    clang
-    turso-cli
-    sqlite
-    mysql84
-    postgresql_16
-    mariadb_110
-    webkitgtk
-    librsvg
-    cairo
-    gdk-pixbuf
-    openssl_3
-    libsoup
-    pkg-config
-    appimagekit
   ];
 }
