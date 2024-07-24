1d63181dd4c354f2365543fd4fc847ef68100e32
Jakku Night <R101g117l117s@gmail.com>
2024-07-24 16:43:15 -0400

    Reconfigure Cloudflare WARP VPN service.

--- networking/vpn-connections/cloudflare-warp.nix
+++ networking/vpn-connections/cloudflare-warp.nix
@@ -1,7 +1,10 @@
 # Install the cloudflare Warp client:
 { config, lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    cloudflare-warp
-  ];
+  services.cloudflare-warp = {
+    enable = true;
+    openFirewall = true;
+    udpPort = 2408;
+    rootDir = "/var/lib/cloudflare-warp";
+  };
 }
