3f818647695119f63afc1f388417d12b2f3667ab
Jakku Night <R101g117l117s@gmail.com>
2024-07-31 17:19:01 -0400

    Add new config.

--- users/jakku/apps/nvim/default.nix
+++ users/jakku/apps/nvim/default.nix
@@ -1,13 +1,32 @@
 # Config module for Neovim/Nixvim:
 { config, lib, pkgs, ... }@inputs:
 {
+  imports = [
+    ./ui/filefinder.nix
+    ./ui/statusline.nix
+    ./ui/buffertab.nix
+    ./ui/filemanager.nix
+    ./ui/whichkey.nix
+    ./ui/dashboard.nix
+    ./syntax-highlighting
+    ./autocompletions
+    ./lsp
+  ];
   programs.nixvim = {
     enable = true;
-    imports = [
-      ./ui/filefinder.nix
-      ./ui/statusline.nix
-      ./ui/buffertab.nix
-      ./ui/filemanager.nix
-    ];
+    globals = {
+      mapleader = " ";
+    };
+    opts = {
+      number = true;
+      relativenumber = false;
+      shiftwidth = 2;
+    };
+    colorschemes.tokyonight = {
+      enable = true;
+      settings = {
+        style = "night";
+      };
+    };
   };
 }
