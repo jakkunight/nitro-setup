7018e2849877bf6bf43112ab0ae5d66976e88ae2
Jakku Night <R101g117l117s@gmail.com>
2024-08-11 20:53:25 -0400

    Delete unused packages.

--- users/jakku/user.nix
+++ users/jakku/user.nix
@@ -11,42 +11,6 @@
     isNormalUser = true;
     initialPassword = "1234";
     extraGroups = [ "wheel" "networkmanager" "libvirtd" ]; # Enable ‘sudo’ for the user.
-
-    # User packages:
-    packages = with pkgs; [
-      firefox
-      libreoffice
-      brave
-      discord
-      remmina
-      audacity
-      libsForQt5.kdenlive
-      obs-studio
-      alacritty
-      xfce.thunar
-      vlc
-      wofi
-      tokyonight-gtk-theme
-      gimp
-      inkscape
-      krita
-      zathura
-      loupe
-      moodle
-      moodle-dl
-      freetube
-      flatpak
-      ffmpeg
-      flatpak-builder
-      teams-for-linux
-      thunderbird
-      rnote
-      waydroid
-      weylus
-      pandoc
-      texliveFull
-      mpvpaper
-    ];
   };
 
   # Add them to the Nix store trusted users:
