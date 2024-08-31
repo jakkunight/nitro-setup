25586ae2eff7eee06ca3ac0862bbe140a198c259
Jakku Night <R101g117l117s@gmail.com>
2024-08-30 22:59:41 -0400

    Fix bufferline warning.

--- users/jakku/apps/nvim/ui/buffertab.nix
+++ users/jakku/apps/nvim/ui/buffertab.nix
@@ -1,10 +1,44 @@
 {
-  programs.nixvim.plugins.bufferline = {
-    enable = true;
-    settings = {
-      options = {
-	alwaysShowBufferline = true;
+  programs.nixvim = {
+    plugins.bufferline = {
+      enable = true;
+      settings = {
+	options = {
+	  alwaysShowBufferline = true;
+	};
       };
     };
+    keymaps = [
+      {
+      	action = "<cmd>bd<CR>";
+	key = "<leader>bd";
+	mode = "n";
+      }
+      {
+      	action = "<cmd>bn<CR>";
+	key = "<leader>bn";
+	mode = "n";
+      }
+      {
+      	action = "<cmd>bp<CR>";
+	key = "<leader>bp";
+	mode = "n";
+      }
+      {
+      	action = "<cmd>w<CR>";
+	key = "<C-s>";
+	mode = "n";
+      }
+      {
+      	action = "<cmd>q<CR>";
+	key = "<C-q>";
+	mode = "n";
+      }
+      {
+      	action = "<cmd>qa<CR>";
+	key = "<C-Q>";
+	mode = "n";
+      }
+    ];
   };
 }
