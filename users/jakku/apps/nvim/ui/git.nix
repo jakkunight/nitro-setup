2ccd0067c2bbb9a6a88a11ed8acba35e81adc15c
Jakku Night <R101g117l117s@gmail.com>
2024-09-08 00:12:44 -0400

    Add LazyGit support and keymaps.

--- users/jakku/apps/nvim/ui/git.nix
+++ users/jakku/apps/nvim/ui/git.nix
@@ -1,6 +1,21 @@
 { config, pkgs, lib, ... }@inputs:
 {
-  programs.nixvim.plugins = {
-    gitsigns.enable = true;
+  programs.nixvim = {
+    plugins = {
+      gitsigns.enable = true;
+      lazygit = {
+	enable = true;
+      };
+    };
+    keymaps =  [
+      {
+	action = "<cmd>LazyGit<CR>";
+	key = "<leader>gg";
+	mode = "n";
+	options = {
+	  desc = "Open Lazygit";
+	}; 
+      }
+    ];
   };
 }
