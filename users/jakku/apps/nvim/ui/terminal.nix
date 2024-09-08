4a092c6363f85e4e0e115ff734d6f908ff185bd0
Jakku Night <R101g117l117s@gmail.com>
2024-09-08 00:12:01 -0400

    Improve toggleterm support. Add keymaps.

--- users/jakku/apps/nvim/ui/terminal.nix
+++ users/jakku/apps/nvim/ui/terminal.nix
@@ -10,5 +10,31 @@
 	};
       };
     };
+    keymaps = [
+      {
+	action = "<cmd>ToggleTerm direction=horizontal<CR>";
+	key = "<leader>th";
+	mode = "n";
+	options = {
+	  desc = "Open a new terminal horizontally";
+	};
+      }
+      {
+	action = "<cmd>ToggleTerm direction=float<CR>";
+	key = "<leader>tf";
+	mode = "n";
+	options = {
+	  desc = "Open a new terminal floating";
+	};
+      }
+      {
+	action = "<cmd>ToggleTerm direction=vertical<CR>";
+	key = "<leader>tv";
+	mode = "n";
+	options = {
+	  desc = "Open a new terminal vertically";
+	};
+      }
+    ];
   };
 }
