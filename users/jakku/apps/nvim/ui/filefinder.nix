e3af948319f40ca316caa8631d632cae404037c2
Jakku Night <R101g117l117s@gmail.com>
2024-09-08 00:13:09 -0400

    Add more keymaps.

--- users/jakku/apps/nvim/ui/filefinder.nix
+++ users/jakku/apps/nvim/ui/filefinder.nix
@@ -6,8 +6,27 @@
     keymaps = [
       {
 	action = "<cmd>Telescope find_files<CR>";
-	key = "<leader>f";
+	key = "<leader>ff";
 	mode = "n";
+	options = {
+	  desc = "Find file with Telescope";
+	};
+      }
+      {
+	action = "<cmd>Telescope buffers<CR>";
+	key = "<leader>fb";
+	mode = "n";
+	options = {
+	  desc = "Find buffers with Telescope";
+	};
+      }
+      {
+	action = "<cmd>Telescope live_grep<CR>";
+	key = "<leader>fg";
+	mode = "n";
+	options = {
+	  desc = "Perform a live grep with Telescope";
+	};
       }
     ];
   };
