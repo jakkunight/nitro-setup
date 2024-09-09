2da4e776769b6a2d323fde90be8193d9de46309b
Jakku Night <R101g117l117s@gmail.com>
2024-09-09 10:16:38 -0400

    Remove conflicting package.

--- users/jakku/apps/android/rooting/heimdall.nix
+++ users/jakku/apps/android/rooting/heimdall.nix
@@ -1,8 +1,7 @@
 { pkgs, lib, config, ... }@inputs:
 {
   # Install Heimdall, needed to root Samsung devices:
-  home.packages = [
-    pkgs.heimdall
-    pkgs.heimdall-gui
+  home.packages = with pkgs; [
+    heimdall-gui
   ];
 }
