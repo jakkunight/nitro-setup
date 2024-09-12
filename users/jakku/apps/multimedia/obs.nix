15e8547fe3b5bdaee2a9ebb63bc2157c09a7a986
Jakku Night <R101g117l117s@gmail.com>
2024-09-12 07:07:51 -0400

    Add OBS-Studio plugins.

--- users/jakku/apps/multimedia/obs.nix
+++ users/jakku/apps/multimedia/obs.nix
@@ -1,15 +1,16 @@
 # OBS Studio setup:
 { pkgs, lib, config, ... }@inputs:
 {
-  home.packages = with pkgs; [
-    # Straming and screen recording:
-    obs-studio
-    obs-studio-plugins.wlrobs
-    obs-studio-plugins.obs-vkcapture
-    obs-studio-plugins.obs-gstreamer
-    obs-studio-plugins.obs-multi-rtmp
-    obs-studio-plugins.obs-mute-filter
-    obs-studio-plugins.obs-pipewire-audio-capture
-
-  ];
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs; [
+      # Straming and screen recording:
+      obs-studio-plugins.wlrobs
+      obs-studio-plugins.obs-vkcapture
+      obs-studio-plugins.obs-gstreamer
+      obs-studio-plugins.obs-multi-rtmp
+      obs-studio-plugins.obs-mute-filter
+      obs-studio-plugins.obs-pipewire-audio-capture
+    ];
+  };
 }
