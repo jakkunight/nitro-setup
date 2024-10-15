c2245e07019e16bd6fa32209a1cec2065907e445
Jakku Night <R101g117l117s@gmail.com>
2024-10-15 11:32:48 -0300

    Add HTTP and SQL clients. Update Devenv.

--- applications/development.nix
+++ applications/development.nix
@@ -4,9 +4,13 @@
   environment.systemPackages = [
     pkgs.nix-ld
     pkgs.cachix
-    # inputs.devenv-repo.packages."x86_64-linux".devenv
-    pkgs.devenv
+    inputs.devenv-repo.packages."x86_64-linux".devenv
+    # pkgs.devenv
     pkgs.direnv
     pkgs.cargo-binutils
+    pkgs.gobang
+    pkgs.httpie
+    pkgs.httpie-desktop
+    pkgs.bruno
   ];
 }
