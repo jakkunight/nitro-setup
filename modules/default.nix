3dedc258a26c24c3a829fa03a2606211684e7ff3
Jakku Night <R101g117l117s@gmail.com>
2025-01-23 10:07:01 -0300

    Import terminal config. + Add defaults to the module configurations.

--- modules/default.nix
+++ modules/default.nix
@@ -3,10 +3,22 @@
     ./bootloader
     ./disk
     ./audio
+    ./terminal
   ];
   config = {
-    bootloader.grub.enable = lib.mkDefault true;
-    bootloader.systemd.enable = lib.mkDefault false;
+    bootloader = {
+      grub.enable = lib.mkDefault true;
+      systemd.enable = lib.mkDefault false;
+    };
     audio.enable = lib.mkDefault true;
+    terminal = {
+      enable = lib.mkDefault true;
+      multiplexer = {
+        enable = lib.mkDefault true;
+      };
+      prompts = {
+        enable = lib.mkDefault true;
+      };
+    };
   };
 }
