8b4429c847fe37a01ff3f2fbfce36e4d6b32c60c
Jakku Night <R101g117l117s@gmail.com>
2025-01-27 20:50:03 -0300

    Enable StrongSwan service.

--- modules/services/vpns/strongswan/default.nix
+++ modules/services/vpns/strongswan/default.nix
@@ -8,6 +8,7 @@
     };
   };
   config = lib.mkIf config.serv.vpns.strongswan.enable {
+    services.strongswan.enable = true;
     serv.vpns.strongswan = {
       connections.enable = lib.mkDefault true;
     };
