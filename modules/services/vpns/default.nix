e6ea6c8121b8e19d68d12378cecbec13a8e4cb62
Jakku Night <R101g117l117s@gmail.com>
2025-01-29 22:00:46 -0300

    Fix VPN config variable.

--- modules/services/vpns/default.nix
+++ modules/services/vpns/default.nix
@@ -7,7 +7,7 @@
       enable = lib.mkEnableOption "Enable VPN services.";
     };
   };
-  config = lib.mkIf config.serv.vpns {
+  config = lib.mkIf config.serv.vpns.enable {
     serv.vpns = {
       strongswan.enable = lib.mkDefault true;
     };
