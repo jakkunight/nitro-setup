6f273874037440c8c42ed4269634c8c629ba36f4
Jakku Night <R101g117l117s@gmail.com>
2025-02-14 19:25:00 -0300

    Update Kitty config. + Set font to Hack Nerd Font (14pt). - Disable audio bell. + Enable shell integration.

--- users/jakku/apps/terminal/kitty.nix
+++ users/jakku/apps/terminal/kitty.nix
@@ -3,5 +3,18 @@
 {
   programs.kitty = {
     enable = true;
+    font = {
+      name = "HackNerdFont";
+      size = 14;
+      package = pkgs.nerd-fonts.hack;
+    };
+    settings = {
+      enable_audio_bell = false;
+    };
+    shellIntegration = {
+      enableZshIntegration = false;
+      enableBashIntegration = false;
+      enableFishIntegration = false;
+    };
   };
 }
