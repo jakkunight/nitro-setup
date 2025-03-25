8653b6de37e3db2c5cbabbfe8f4617b994097d21
Jakku Night <R101g117l117s@gmail.com>
2025-03-25 09:21:58 -0300

    Add MPD service. + Add and configure MPD service.

--- modules/audio/mpd.nix
+++ modules/audio/mpd.nix
@@ -19,15 +19,9 @@
           name "My PipeWire Output"
         }
       '';
-
-      # Optional:
-      network = {
-        listenAddress = "any"; # if you want to allow non-localhost connections
-        startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
-      };
     };
     systemd.services.mpd.environment = {
-      XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.userRunningPipeWire.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
+      XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.${username}.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
     };
   };
 }
