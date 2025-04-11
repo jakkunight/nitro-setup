cb8d80d42343780dbae1fdbd6c49aec3670adfe3
Jakku Night <R101g117l117s@gmail.com>
2025-04-11 08:57:11 -0300

    Update flake.nix + Add substituters midules. - Remove substituters settings from the flake fields.

--- flake.nix
+++ flake.nix
@@ -3,20 +3,6 @@
     My Setup Settings (v3.0.0)
   '';
 
-  # Enable Cachix:
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://devenv.cachix.org"
-      "https://hyprland.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
-
   inputs = {
     # Nixpkgs:
     nixpkgs = {
@@ -107,6 +93,31 @@
             ...
           }: {
             environment.etc."nixos-config".source = ./.;
+          })
+          # Nix settings:
+          ({config, ...}: {
+            # Configure Nix:
+            nix.settings = {
+              # Optimise Nix-Store:
+              auto-optimise-store = true;
+
+              # Enable Flakes:
+              experimental-features = ["nix-command" "flakes"];
+
+              # Use binary cache:
+              extra-substituters = [
+                "https://nix-community.cachix.org"
+                "https://devenv.cachix.org"
+                "https://hyprland.cachix.org"
+                "https://yazi.cachix.org"
+              ];
+              extra-trusted-public-keys = [
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+              ];
+            };
           })
           # NVF:
           inputs.nvf.nixosModules.default
