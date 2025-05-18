939c7caf96a5098b7bd35d4a73175e945e97ae6e
Jakku Night <R101g117l117s@gmail.com>
2025-05-18 20:19:17 -0300

    Update bufferline. + Switch to mini.tabline.

--- modules/system/terminal/editor/nvim/nvf/ui/bufferline/default.nix
+++ modules/system/terminal/editor/nvim/nvf/ui/bufferline/default.nix
@@ -1,13 +1,27 @@
-{ lib, config, inputs, ... }:
-{
-  config = {
-    programs.nvf.settings.vim = {
-      tabline.nvimBufferline = {
-        enable = true;
-        mappings = {
-          closeCurrent = "<leader>bx";
-        };
-      };
+_: {
+  programs.nvf.settings.vim = {
+    mini.tabline = {
+      enable = true;
     };
+    keymaps = [
+      {
+        key = "<leader>-";
+        action = ":bd!<CR>";
+        mode = ["n"];
+        desc = "Close current buffer.";
+      }
+      {
+        key = "<leader>.";
+        action = ":bn<CR>";
+        mode = ["n"];
+        desc = "Cycle next buffer.";
+      }
+      {
+        key = "<leader>,";
+        action = ":bp<CR>";
+        mode = ["n"];
+        desc = "Cycle prev buffer.";
+      }
+    ];
   };
 }
