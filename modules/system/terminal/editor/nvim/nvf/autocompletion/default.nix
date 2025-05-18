9248f1af3105b041a8bff4fb9b55eeb74b03b46f
Jakku Night <R101g117l117s@gmail.com>
2025-05-18 20:02:46 -0300

    Set autocompletion engine to mini.completion.

--- modules/system/terminal/editor/nvim/nvf/autocompletion/default.nix
+++ modules/system/terminal/editor/nvim/nvf/autocompletion/default.nix
@@ -7,74 +7,41 @@ _: {
       comment = {
         enable = true;
       };
-      indentscope = {
-        enable = true;
-      };
       operators = {
         enable = true;
       };
       pairs = {
         enable = true;
       };
-    };
-
-    lsp.lspkind = {
-      enable = true;
-    };
-
-    autocomplete = {
-      enableSharedCmpSources = true;
-      blink-cmp = {
+      snippets = {
         enable = true;
-        friendly-snippets.enable = true;
-        setupOpts = {
-          auto_show = true;
-          sources = {
-            default = [
-              "lsp"
-              "path"
-              "snippets"
-              "buffer"
-              "omni"
-              "spell"
-              "ripgrep"
-            ];
-          };
-          providers = {
-            lsp = {
-              enable = true;
-              module = "";
-              score_offset = 100;
-            };
-            path = {
-              enable = true;
-              score_offset = 200;
-            };
-            snippets = {
-              enable = true;
-              score_offset = 300;
-            };
-            buffer = {
-              enable = true;
-              score_offset = 400;
-            };
-            omni = {
-              enable = true;
-              score_offset = 600;
-            };
-          };
-        };
-        sourcePlugins = {
-          ripgrep = {
-            enable = true;
-            package = "blink-ripgrep-nvim";
-          };
-          spell = {
-            enable = true;
-            package = "blink-cmp-spell";
-          };
-        };
+      };
+      surround = {
+        enable = true;
+      };
+      completion = {
+        enable = true;
       };
     };
+    keymaps = [
+      {
+        key = "<Tab>";
+        mode = ["i"];
+        action = "pumvisible() ? \"\\<C-n>\" : \"\\<Tab>\"";
+        expr = true;
+      }
+      {
+        key = "<S-Tab>";
+        mode = ["i"];
+        action = "pumvisible() ? \"\\<C-p>\" : \"\"";
+        expr = true;
+      }
+      {
+        key = "<CR>";
+        mode = ["i"];
+        action = "pumvisible() ? \"\\<C-y>\" : \"\\<CR>\"";
+        expr = true;
+      }
+    ];
   };
 }
