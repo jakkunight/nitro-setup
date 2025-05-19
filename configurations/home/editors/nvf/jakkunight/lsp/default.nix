eb042b850c47c6e1738415c40a04d2520f8cb418
Jakku Night <R101g117l117s@gmail.com>
2025-05-19 13:03:43 -0300

    Update LSP config. + Update LSP config.

--- configurations/home/editors/nvf/jakkunight/lsp/default.nix
+++ configurations/home/editors/nvf/jakkunight/lsp/default.nix
@@ -1,13 +1,12 @@
 _: {
   programs.nvf.settings.vim.lsp = {
+    enable = true;
     formatOnSave = true;
     lspsaga.enable = true;
-    lspkind.enable = true;
     trouble.enable = true;
     inlayHints.enable = true;
     lspSignature.enable = true;
     lspconfig.enable = true;
-    null-ls.enable = true;
     nvim-docs-view.enable = true;
   };
 }
