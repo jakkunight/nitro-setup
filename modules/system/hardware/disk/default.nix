8d3e50a82a46c24b4a1651d8b6f0e888f020ed2a
Jakku Night <R101g117l117s@gmail.com>
2025-05-26 04:10:42 -0300

    Update disk module. + Change Disko config.
    TODO:
    + Need to finish and clean the config.

--- modules/system/hardware/disk/default.nix
+++ modules/system/hardware/disk/default.nix
@@ -1,11 +1,34 @@
 {
   config,
   lib,
+  inputs,
   ...
-}: {
-  # Load the disko layout
-  imports = [
-    ./disko
+}: let
+  # Define a name for each custom disko configuration
+  layouts = {
+    simple = ./configs/simple-layout.nix;
+  };
+  selectedLayout = layouts.${config.modules.system.hardware.disk.disko.layout} {
+    device-target = config.modules.system.hardware.disk.disko.device;
+  };
+in {
+  options = {
+    modules.system.hardware.disk.disko = {
+      layout = lib.mkOptionType {
+        type = lib.types.enum (builtins.attrNames layouts);
+        default = "simple";
+        description = "Disk layout to use with Disko";
+      };
+      device = lib.mkOptionType {
+        type = lib.types.nonEmptyStr;
+        default = "/dev/nvme0n1";
+        description = "Set the instalation drive for disko";
+      };
+    };
+  };
+  imports = lib.optionals (layouts ? ${config.modules.system.hardware.disk.disko.layout}) [
+    inputs.disko.nixosModules.disko
+    selectedLayout
   ];
   # Loads ALL the drivers for mass storage devices
   boot.initrd.availableKernelModules = [
