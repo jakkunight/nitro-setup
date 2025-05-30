a62000155dbad6d6b476030cc2683ba7251f5696
Jakku Night <R101g117l117s@gmail.com>
2025-05-30 09:08:16 -0300

    Add disk forensic tools.

--- modules/system/disk/tools/default.nix
+++ modules/system/disk/tools/default.nix
@@ -19,6 +19,20 @@
       pkgs.dua
       pkgs.exfatprogs
       pkgs.exfat
+      pkgs.testdisk
+      pkgs.ddrescue
+      pkgs.burp
+      pkgs.foremost
+      pkgs.ntfs3g
+      pkgs.memtester
+      pkgs.rsync
+      pkgs.sleuthkit
+      pkgs.autopsy
+      pkgs.chkrootkit
+      pkgs.unhide
+      pkgs.unhide-gui
+      pkgs.safecopy
+      pkgs.gptfdisk
     ];
   };
 }
