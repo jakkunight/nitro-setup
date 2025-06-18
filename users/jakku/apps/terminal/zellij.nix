4f74cf32159e701f45d50e4b8d71727508f90aa4
Jakku Night <R101g117l117s@gmail.com>
2025-06-18 07:19:31 -0300

    Configure Zjstatus plugin.

--- users/jakku/apps/terminal/zellij.nix
+++ users/jakku/apps/terminal/zellij.nix
@@ -1,6 +1,11 @@
 # Zellij config:
-{config, lib, pkgs, ...}@inputs:
 {
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.zellij = {
     enable = true;
     enableZshIntegration = false;
@@ -23,6 +28,44 @@
           orange = "#ff9e64";
         };
       };
+      pane_frames = false;
+      default_shell = "zsh";
     };
   };
+  xdg.configFile."zellij/layouts/default.kdl".text = ''
+    layout {
+      default_tab_template {
+        children
+          pane size=2 borderless=true {
+            plugin location="file:${inputs.zjstatus.packages."x86_64-linux".default}/bin/zjstatus.wasm" {
+              format_left   "{mode} #[bold]{session}{tabs}"
+              format_center ""
+              format_right  "{command_git_branch} {datetime}"
+              format_space  "|"
+
+              border_enabled  "true"
+              border_char     "─"
+              border_format   "#[fg=#6C7086]{char}"
+              border_position "top"
+
+              hide_frame_for_single_pane "true"
+
+              mode_normal  "#[bg=blue] "
+              mode_tmux    "#[bg=#ffc387] "
+
+              tab_normal   "#[fg=#6C7086] {name} "
+              tab_active   "#[fg=#9399B2,bold,italic] {name} "
+
+              command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
+              command_git_branch_format      "#[fg=blue] {stdout} "
+              command_git_branch_interval    "10"
+              command_git_branch_rendermode  "static"
+
+              datetime        "#[fg=#6C7086,bold] {format} "
+              datetime_format "%A, %d %b %Y %H:%M"
+              datetime_timezone "America/Asuncion"
+            }
+          }
+        }
+    }'';
 }
