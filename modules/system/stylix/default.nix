8417f8a970dae6f168d2c7ebfbf9af0fed5ec085
Jakku Night <R101g117l117s@gmail.com>
2025-06-19 07:26:31 -0300

    Add initial Stylix support.

---
+++ modules/system/stylix/default.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyonight-night";
+    image = ./scaramouche-sealed-wallpaper.jpg;
+    polarity = "dark";
+    fonts = {
+      serif = {
+        package = inputs.genshin-font.packages.${pkgs.system}.default;
+        name = "Genshin Impact";
+      };
+
+      sansSerif = {
+        package = inputs.genshin-font.packages.${pkgs.system}.default;
+        name = "Genshin Impact";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.hack;
+        name = "Hack Nerd Font Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
+}
