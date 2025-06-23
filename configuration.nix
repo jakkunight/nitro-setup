982748c98373ec944d506af6d92201164c0bfd65
Jakku Night <J97k107u45k117n@gmail.com>
2025-06-23 16:48:48 -0300

    Add basic programs to migrate the setup...

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,9 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     nh
+    home-manager
+    disko
+    git
   ];
 
   # UWSM:
