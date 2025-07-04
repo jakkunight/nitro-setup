98e995fbcfe37017c6919f947428c398a8c9baa8
Jakku Night <J97k107u45k117n@gmail.com>
2025-07-04 09:55:22 -0300

    Fix DNS resolution for split-tunneling.

--- modules/nixos/strongswan/default.nix
+++ modules/nixos/strongswan/default.nix
@@ -1,45 +1,33 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }: let
-  secretsPath = config.sops.secrets;
-  default_table = "220";
   updown_script = {
     content = ''
       echo "[UPDOWN] Initilizing script..."
-      DEFAULT_GATEWAY=$(${pkgs.iproute2}/bin/ip route show | grep -i 'default via'| ${pkgs.gawk}/bin/awk '{print $3 }')
-      DEV_IFACE=$(${pkgs.iproute2}/bin/ip addr show | ${pkgs.gawk}/bin/awk '/inet.*brd/{print $NF; exit}')
-      VPN_SUBNET_1=$(cat ${secretsPath."andescada/vpn_subnet_1".path})
-      VPN_SUBNET_2=$(cat ${secretsPath."andescada/vpn_subnet_2".path})
-
       case "$PLUTO_VERB" in
         up-client)
-          echo "[UPDOWN] Cleaning up route tables..."
-          # sudo ip route flush table ${default_table}
-          echo "[UPDOWN] Configuring split-tunnelling..."
-          ${pkgs.iproute2}/bin/ip route add $VPN_SUBNET_1 via $DEFAULT_GATEWAY src $PLUTO_MY_SOURCE_IP dev $DEV_IFACE proto static table ${default_table}
-          ${pkgs.iproute2}/bin/ip route add $VPN_SUBNET_2 via $DEFAULT_GATEWAY src $PLUTO_MY_SOURCE_IP dev $DEV_IFACE proto static table ${default_table}
-          ${pkgs.iproute2}/bin/ip route del default table ${default_table}
-          ${pkgs.iproute2}/bin/ip route add 10.0.0.0/8 via $DEFAULT_GATEWAY dev $DEV_IFACE src $PLUTO_ME proto dhcp table ${default_table}
-          echo "[UPDOWN] Replacing default DNS resolver..."
-          rm /etc/resolv.conf
-          echo "nameserver $DEFAULT_GATEWAY" > /etc/resolv.conf
-          echo "[UPDOWN] Restarting NetworkManager..."
-          systemctl restart NetworkManager
-          echo "[UPDOWN] Success!"
+          echo "[INFO] Turning up IPSec Client..."
+          echo "[INFO] Getting interface..."
+          iface=lo.ipsec
+          echo "[INFO] Removing DNS resolution via VPN DNS Server..."
+          ${pkgs.openresolv}/bin/resolvconf -d $iface
+          if [ $? -ne 0 ]; then
+            echo "[WARN] Could not remove DNS resolution via VPN DNS Server."
+            echo "[WARN] You can still use the VPN, but some domains might be"
+            echo "[WARN] unaccessable due to security policies."
+          else
+             echo "[SUCCESS] VPN DNS Server resolution removed!"
+          fi
+          echo "[SUCCESS] The IPSec Client is UP!"
           ;;
         down-client)
-          echo "[UPDOWN] Erasing extra routing tables..."
-          ${pkgs.iproute2}/bin/ip route del $VPN_SUBNET_1 table ${default_table}
-          ${pkgs.iproute2}/bin/ip route del $VPN_SUBNET_2 table ${default_table}
-          ${pkgs.iproute2}/bin/ip route del 0.0.0.0/0 table ${default_table}
-          ${pkgs.iproute2}/bin/ip route flush table ${default_table}
-          echo "[UPDOWN] Success!"
+          echo "[INFO] Turning down IPSec Client..."
+          echo "[SUCCESS] The IPSec Client is DOWN!"
           ;;
       esac
-      echo "[UPDOWN] The leftupdown command was successfully executed."
+      echo "[SUCCESS] The leftupdown command was successfully executed."
     '';
     path = "ipsec.d/updown.sh";
   };
@@ -103,7 +91,7 @@ in {
         leftauth2 = "xauth";
         xauth_identity = "santiago_wu";
         auto = "add";
-        # leftupdown = "/etc/${updown_script.path}";
+        leftupdown = "/etc/${updown_script.path}";
       };
     };
   };
