920cef25fe4442f8b2cdd8cbd8aac6b0f7a355bd
Jakku Night <J97k107u45k117n@gmail.com>
2025-07-09 11:55:36 -0300

    Add Zed-Editor support.

---
+++ modules/home-manager/zed-editor/default.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  programs.zed-editor = {
+    enable = true;
+    userSettings = {
+      vim_mode = true;
+    };
+    userKeymaps = {};
+    extensions = [
+      "nix"
+    ];
+  };
+}
