3d0c4ca780c73f74567f316c46b0650e0ff2a98a
Jakku Night <J97k107u45k117n@gmail.com>
2025-07-11 09:29:19 -0300

    Update Helix config. + Add more diagnostics. + Add support for Markdown. + Improve completions.

--- modules/home-manager/helix/default.nix
+++ modules/home-manager/helix/default.nix
@@ -1,17 +1,44 @@
 {pkgs, ...}: {
+  # Install packages to the "$PATH":
+  home.packages = with pkgs; [
+    mdformat
+    markdown-oxide
+  ];
   programs.helix = {
     enable = true;
     defaultEditor = true;
     settings = {
       theme = "tokyonight";
       editor = {
+        cursorline = true;
+        auto-completion = true;
+        path-completion = true;
+        auto-format = true;
+        bufferline = "always";
         line-number = "relative";
         lsp = {
+          snippets = true;
+          display-progress-messages = true;
           display-messages = true;
           display-inlay-hints = true;
         };
+        inline-diagnostics = {
+          cursor-line = "warning";
+          other-lines = "info";
+        };
+        end-of-line-diagnostics = "hint";
         indent-guides = {
           render = true;
+        };
+        clipboard-provider = "wayland";
+        gutters = {
+          layout = [
+            "diagnostics"
+            "spacer"
+            "line-numbers"
+            "spacer"
+            "diff"
+          ];
         };
       };
       keys = {};
@@ -37,9 +64,16 @@
           name = "markdown";
           auto-format = true;
           formatter = {
-            command = "${pkgs.mdformat}/bin/mdformmat";
-            args = ["--stdin"];
+            command = "${pkgs.mdformat}/bin/mdformat";
+            args = [
+              "-"
+            ];
           };
+          language-servers = [
+            {
+              name = "markdown-oxide";
+            }
+          ];
         }
       ];
     };
