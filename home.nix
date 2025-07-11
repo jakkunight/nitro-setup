32ab8480307343edbd9a4b7aad0511ff8ba4ec9f
Jakku Night <J97k107u45k117n@gmail.com>
2025-07-11 11:08:45 -0300

    Refactor `home.packages`.

--- home.nix
+++ home.nix
@@ -41,7 +41,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -58,12 +58,12 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    pkgs.nemo
-    pkgs.vlc
-    pkgs.remmina
-    pkgs.loupe
-    pkgs.devenv
-    pkgs.direnv
+    nemo
+    vlc
+    remmina
+    loupe
+    devenv
+    direnv
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
