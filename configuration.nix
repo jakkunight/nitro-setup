fe864284d4dfedb091f3cde332c4c58376234396
Jakku Night <J97k107u45k117n@gmail.com>
2025-07-11 11:09:24 -0300

    Multiple changes. + Add RipGrep support. + Set hostname to "nitro".

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@
   # };
   # ```
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nitro"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
@@ -133,6 +133,7 @@
     zoxide
     eza
     bat
+    ripgrep
     sops
     dua
     remmina
