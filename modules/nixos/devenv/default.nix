27a6da30cc890052cca309654f5a61d1c75db1bb
Jakku Night <J97k107u45k117n@gmail.com>
2025-07-12 00:17:32 -0300

    Fix Direnv setup.

--- modules/nixos/devenv/default.nix
+++ modules/nixos/devenv/default.nix
@@ -16,8 +16,15 @@
     ];
   };
 
+  programs.nix-ld.enable = true;
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     devenv
-    direnv
   ];
 }
