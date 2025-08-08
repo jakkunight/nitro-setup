461725b7884e4f194408a539c2b06607de12e922
Jakku Night <J97k107u45k117n@gmail.com>
2025-08-08 11:11:53 -0300

    Change color scheme to Kanagawa.

--- modules/nixos/sddm/default.nix
+++ modules/nixos/sddm/default.nix
@@ -1,9 +1,10 @@
-_: {
+{pkgs, ...}: {
   # SDDM:
   services.displayManager.sddm = {
     enable = true;
     enableHidpi = true;
     wayland.enable = true;
     wayland.compositor = "kwin";
+    theme = "${pkgs.sddm-astronaut.override {embeddedTheme = "japanese_aesthetic";}}/share/sddm/themes/sddm-astronaut-theme";
   };
 }
