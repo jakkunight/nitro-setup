ba94b8fb9b799d9a6ea409a599052765f360a54e
Jakku Night <J97k107u45k117n@gmail.com>
2025-08-13 21:14:16 -0300

    Fix config.

--- modules/home-manager/presenterm/default.nix
+++ modules/home-manager/presenterm/default.nix
@@ -1,22 +1,28 @@
-{pkgs, ...}: let
-  yamlFormat = pkgs.formats.yaml;
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  yamlFormat = pkgs.formats.yaml {};
   presentermConfigFile = "config.yaml";
   presentermConfig = {
-    theme = "terminal-dark";
+    defaults = {
+      theme = "terminal-dark";
+    };
     transition = {
       duration_millis = 1000;
       frames = 60;
       animation = {
         style = "fade";
       };
-      # [WARN] Use this at YOUR OWN RISK!!!
-      snippet = {
-        exec = {
-          enable = true;
-        };
-        exec_replace = {
-          enable = true;
-        };
+    };
+    # [WARN] Use this at YOUR OWN RISK!!!
+    snippet = {
+      exec = {
+        enable = true;
+      };
+      exec_replace = {
+        enable = true;
       };
     };
   };
@@ -26,8 +32,17 @@ in {
       presenterm
       mermaid-cli
     ];
-    xdg.configFile."presenterm/${presentermConfigFile}" = {
-      source = yamlFormat.generate presentermConfigFile presentermConfig;
-    };
+  };
+  xdg.configFile."presenterm/${presentermConfigFile}" = {
+    source = (yamlFormat.generate presentermConfigFile presentermConfig).overrideAttrs (
+      # [NOTE] This was taken from home-manager/alacritty module for reference...
+      finalAttrs: prevAttrs: {
+        buildCommand = lib.concatStringsSep "\n" [
+          prevAttrs.buildCommand
+          # TODO: why is this needed? Is there a better way to retain escape sequences?
+          "substituteInPlace $out --replace-quiet '\\\\' '\\'"
+        ];
+      }
+    );
   };
 }
