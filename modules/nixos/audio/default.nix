5fbe68d299cabf09d3a5adacbc1b915568f90663
Jakku Night <J97k107u45k117n@gmail.com>
2025-08-24 10:08:50 -0300

    Add some audio utilities. + Add ALSA utils. + Enable JACK audio.

--- modules/nixos/audio/default.nix
+++ modules/nixos/audio/default.nix
@@ -3,8 +3,18 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    jack.enable = true;
   };
-  environment.systemPackages = [
-    pkgs.pavucontrol
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+    alsa-firmware
+    alsa-utils
   ];
+  boot.extraModprobeConfig = ''
+    options snd-hda-intel model=auto
+  '';
 }
