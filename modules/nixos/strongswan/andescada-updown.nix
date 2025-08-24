e90468628cafd93121a421a4f2551d6dca4ce978
Jakku Night <J97k107u45k117n@gmail.com>
2025-08-24 10:08:12 -0300

    Update strongswan updown script. + Update andescada updown script.

--- modules/nixos/strongswan/andescada-updown.nix
+++ modules/nixos/strongswan/andescada-updown.nix
@@ -1,44 +1,26 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
-  secretsPath = config.sops.secrets;
-  ip = "${pkgs.iproute2}/bin/ip";
-  awk = "${pkgs.gawk}/bin/awk";
-  default_table = "220";
-in
-  pkgs.writeShellScriptBin "andescada-updown.sh" ''
-    echo "[UPDOWN] Initilizing script..."
-    DEFAULT_GATEWAY=$(${ip} route show | grep -i 'default via'| ${awk} '{print $3 }')
-    DEV_IFACE=$(${ip} addr show | ${awk} '/inet.*brd/{print $NF; exit}')
-    VPN_SUBNET_1=$(cat '${secretsPath."andescada/vpn_subnet_1".path}')
-    VPN_SUBNET_2=$(cat '${secretsPath."andescada/vpn_subnet_2".path}')
-
-    case "$PLUTO_VERB" in
-      up-client)
-        echo "[UPDOWN] Cleaning up route tables..."
-        # sudo ip route flush table ${default_table}
-        echo "[UPDOWN] Configuring split-tunnelling..."
-        sudo ${ip} route add $VPN_SUBNET_1 via $DEFAULT_GATEWAY src $PLUTO_MY_SOURCE_IP dev $DEV_IFACE proto static table ${default_table}
-        sudo ${ip} route add $VPN_SUBNET_2 via $DEFAULT_GATEWAY src $PLUTO_MY_SOURCE_IP dev $DEV_IFACE proto static table ${default_table}
-        sudo ${ip} route del default table ${default_table}
-        sudo ${ip} route add 10.0.0.0/8 via $DEFAULT_GATEWAY dev $DEV_IFACE src $PLUTO_ME proto dhcp table ${default_table}
-        echo "[UPDOWN] Replacing default DNS resolver..."
-        sudo rm /etc/resolv.conf
-        sudo echo "nameserver $DEFAULT_GATEWAY" > /etc/resolv.conf
-        echo "[UPDOWN] Restarting NetworkManager..."
-        sudo systemctl restart NetworkManager
-        echo "[UPDOWN] Success!"
-        ;;
-      down-client)
-        echo "[UPDOWN] Erasing extra routing tables..."
-        sudo ${ip} route del $VPN_SUBNET_1 table ${default_table}
-        sudo ${ip} route del $VPN_SUBNET_2 table ${default_table}
-        sudo ${ip} route del 0.0.0.0/0 table ${default_table}
-        sudo ${ip} route flush table ${default_table}
-        echo "[UPDOWN] Success!"
-        ;;
-    esac
-    echo "[UPDOWN] The leftupdown command was successfully executed."
-  ''
+{pkgs, ...}:
+pkgs.writeShellScript "andescada-updown.sh" ''
+  echo "[UPDOWN] Initilizing script..."
+  case "$PLUTO_VERB" in
+    up-client)
+      echo "[INFO] Turning up IPSec Client..."
+      echo "[INFO] Getting interface..."
+      iface=lo.ipsec
+      echo "[INFO] Removing DNS resolution via VPN DNS Server..."
+      ${pkgs.openresolv}/bin/resolvconf -d $iface
+      if [ $? -ne 0 ]; then
+        echo "[WARN] Could not remove DNS resolution via VPN DNS Server."
+        echo "[WARN] You can still use the VPN, but some domains might be"
+        echo "[WARN] unaccessable due to security policies."
+      else
+         echo "[SUCCESS] VPN DNS Server resolution removed!"
+      fi
+      echo "[SUCCESS] The IPSec Client is UP!"
+      ;;
+    down-client)
+      echo "[INFO] Turning down IPSec Client..."
+      echo "[SUCCESS] The IPSec Client is DOWN!"
+      ;;
+  esac
+  echo "[SUCCESS] The leftupdown command was successfully executed."
+''
--- modules/nixos/strongswan/default.nix
+++ modules/nixos/strongswan/default.nix
@@ -3,34 +3,31 @@
   pkgs,
   ...
 }: let
-  updown_script = {
-    content = ''
-      echo "[UPDOWN] Initilizing script..."
-      case "$PLUTO_VERB" in
-        up-client)
-          echo "[INFO] Turning up IPSec Client..."
-          echo "[INFO] Getting interface..."
-          iface=lo.ipsec
-          echo "[INFO] Removing DNS resolution via VPN DNS Server..."
-          ${pkgs.openresolv}/bin/resolvconf -d $iface
-          if [ $? -ne 0 ]; then
-            echo "[WARN] Could not remove DNS resolution via VPN DNS Server."
-            echo "[WARN] You can still use the VPN, but some domains might be"
-            echo "[WARN] unaccessable due to security policies."
-          else
-             echo "[SUCCESS] VPN DNS Server resolution removed!"
-          fi
-          echo "[SUCCESS] The IPSec Client is UP!"
-          ;;
-        down-client)
-          echo "[INFO] Turning down IPSec Client..."
-          echo "[SUCCESS] The IPSec Client is DOWN!"
-          ;;
-      esac
-      echo "[SUCCESS] The leftupdown command was successfully executed."
-    '';
-    path = "/etc/ipsec.d/updown.sh";
-  };
+  andescada-updown = ''
+    echo "[UPDOWN] Initilizing script..."
+    case "$PLUTO_VERB" in
+      up-client)
+        echo "[INFO] Turning up IPSec Client..."
+        echo "[INFO] Getting interface..."
+        iface=lo.ipsec
+        echo "[INFO] Removing DNS resolution via VPN DNS Server..."
+        ${pkgs.openresolv}/bin/resolvconf -d $iface
+        if [ $? -ne 0 ]; then
+          echo "[WARN] Could not remove DNS resolution via VPN DNS Server."
+          echo "[WARN] You can still use the VPN, but some domains might be"
+          echo "[WARN] unaccessable due to security policies."
+        else
+           echo "[SUCCESS] VPN DNS Server resolution removed!"
+        fi
+        echo "[SUCCESS] The IPSec Client is UP!"
+        ;;
+      down-client)
+        echo "[INFO] Turning down IPSec Client..."
+        echo "[SUCCESS] The IPSec Client is DOWN!"
+        ;;
+    esac
+    echo "[SUCCESS] The leftupdown command was successfully executed."
+  '';
 in {
   # Set the secrets up:
   sops.secrets = {
@@ -60,11 +57,10 @@ in {
     networkmanager_strongswan
   ];
 
-  # Write the script to /etc/ipsec.d
   environment.etc = {
-    "${updown_script.path}" = {
-      text = updown_script.content;
-      mode = "770";
+    "ipsec.d/andescada/updown.sh" = {
+      text = andescada-updown;
+      mode = "0770";
     };
   };
 
@@ -114,7 +110,7 @@ in {
         leftauth2 = "xauth";
         xauth_identity = "santiago_wu";
         auto = "add";
-        leftupdown = "${updown_script.path}";
+        leftupdown = "/etc/ipsec.d/andescada/updown.sh";
       };
     };
   };
