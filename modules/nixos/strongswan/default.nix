93a7e3754c01f62e1ad7b741d7a85bdd564bf10d
Jakku Night <J97k107u45k117n@gmail.com>
2025-09-24 19:27:17 -0300

    Update andescada configs. + Update nameserver DNS server to Cloudflare.

--- modules/nixos/strongswan/default.nix
+++ modules/nixos/strongswan/default.nix
@@ -11,7 +11,7 @@
         echo "[INFO] Getting interface..."
         iface=lo.ipsec
         echo "[INFO] Removing DNS resolution via VPN DNS Server..."
-        echo "nameserver 94.140.14.14" | ${pkgs.openresolv}/bin/resolvconf -a $iface
+        echo "nameserver 1.1.1.1" | ${pkgs.openresolv}/bin/resolvconf -a $iface
         if [ $? -ne 0 ]; then
           echo "[WARN] Could not remove DNS resolution via VPN DNS Server."
           echo "[WARN] You can still use the VPN, but some domains might be"
