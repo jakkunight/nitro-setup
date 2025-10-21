72572a5d52e7095d5caa1640da25d3837e6a361e
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-21 18:21:37 -0300

    Revert "Make boot logs verbose + Increase the verbosity of the boot logs to debug boot issues."
    This reverts commit 97423982e74180792c17e764a7abffec274f1a9c.

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -12,23 +12,20 @@
     systemd-udev-settle.enable = false;
     NetworkManager-wait-online.enable = false;
   };
-  boot = {
-    consoleLogLevel = 7;
-    loader = {
-      efi = {
-        canTouchEfiVariables = true;
-      };
-      systemd-boot = {
-        enable = false;
-      };
-      grub = {
-        enable = true;
-        efiSupport = true;
-        device = "nodev";
-        useOSProber = true;
-        efiInstallAsRemovable = false;
-        theme = lib.mkForce "${inputs.wanderer-grub-theme.packages.${pkgs.system}.grub-theme}";
-      };
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+    };
+    systemd-boot = {
+      enable = false;
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = true;
+      efiInstallAsRemovable = false;
+      theme = lib.mkForce "${inputs.wanderer-grub-theme.packages.${pkgs.system}.grub-theme}";
     };
   };
 }
