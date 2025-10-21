97423982e74180792c17e764a7abffec274f1a9c
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-21 10:13:29 -0300

    Make boot logs verbose + Increase the verbosity of the boot logs to debug boot issues.

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -12,20 +12,23 @@
     systemd-udev-settle.enable = false;
     NetworkManager-wait-online.enable = false;
   };
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-    };
-    systemd-boot = {
-      enable = false;
-    };
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = true;
-      efiInstallAsRemovable = false;
-      theme = lib.mkForce "${inputs.wanderer-grub-theme.packages.${pkgs.system}.grub-theme}";
+  boot = {
+    consoleLogLevel = 7;
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      systemd-boot = {
+        enable = false;
+      };
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
+        efiInstallAsRemovable = false;
+        theme = lib.mkForce "${inputs.wanderer-grub-theme.packages.${pkgs.system}.grub-theme}";
+      };
     };
   };
 }
