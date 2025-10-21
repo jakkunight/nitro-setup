978148d1dae0a39411eaa35e38e8c98a08901fe7
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-21 07:52:09 -0300

    Adjust charging protection + Stop charging the battery at 90% or above.

--- modules/nixos/services/tlp.nix
+++ modules/nixos/services/tlp.nix
@@ -15,7 +15,7 @@ _: {
 
       # Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
     };
   };
 }
