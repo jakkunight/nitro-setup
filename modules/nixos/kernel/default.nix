66198fe2ff88defeaaad82951e5bc02c662704b0
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-21 15:26:46 -0300

    Pin Linux Kernel version + Set Linux Kernel version to 6.16.x. WARNING: - Waitting for 6.17.x to be patched due to errors with NVME devices.

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -2,7 +2,7 @@
   # Use a custom kernel:
   boot.kernel.enable = true;
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_16;
   # Firmware/BIOS updates:
   services.fwupd.enable = true;
 }
