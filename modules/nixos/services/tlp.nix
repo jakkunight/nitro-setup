c803f656b46bd518548e761243d49e0fccba1018
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-31 07:55:07 -0300

    Set battery charging limits to TLP + Set battery charge limits for TLP.
    WARNINIG:
    - These settings are currently being ignored.

--- modules/nixos/services/tlp.nix
+++ modules/nixos/services/tlp.nix
@@ -14,8 +14,8 @@ _: {
       CPU_MAX_PERF_ON_BAT = 20;
 
       # Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
+      START_CHARGE_THRESH_BAT0 = 20; # 20 and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
     };
   };
 }
