9507f1d7d54fda35cc84af78a494e7b4204db511
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-31 07:53:00 -0300

    Set kernel version + Using the latest kernel release from Nixpkgs.

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -2,7 +2,7 @@
   # Use a custom kernel:
   boot.kernel.enable = true;
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_16;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   # Firmware/BIOS updates:
   services.fwupd.enable = true;
 }
