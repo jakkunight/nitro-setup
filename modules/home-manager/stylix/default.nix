db59ece58543de956dc1a0cc2fb88a0b50712941
Jakku Night <J97k107u45k117n@gmail.com>
2025-11-26 21:20:23 -0300

    Add new wallpapers

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -41,7 +41,7 @@
     # };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa-dragon.yaml";
     polarity = "dark";
-    image = ./wallpapers/wanderer-scaramouche-aranaras-wallpaper.jpg;
+    image = ./wallpapers/tanjiro-kamado-gruv.jpg;
     fonts = {
       serif = {
         package = inputs.genshin-font.packages.${pkgs.system}.default;
