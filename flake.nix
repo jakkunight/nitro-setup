8311a5145b85b775ffd39fa907110102cf2045fe
Jakku Night <J97k107u45k117n@gmail.com>
2025-12-13 11:25:27 -0300

    Create basic dendritic flake + Create the basic main flake. + Add support for Determinate Nix. + Add the available system platforms.

--- flake.nix
+++ flake.nix
@@ -1,61 +1,11 @@
+# flake.nix
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
-    import-tree = {
-      url = "github:vic/import-tree";
-    };
-    flake-file = {
-      url = "github:vic/flake-file";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    wanderer-grub-theme = {
-      url = "github:jakkunight/Wanderer-Themes";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+    # all other inputs your flake needs, like nixpkgs.
   };
-
-  outputs = {
-    flake-parts,
-    home-manager,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} ({
-        withSystem,
-        config,
-        lib,
-        ...
-      } @ top: {
-        imports = [
-          flake-parts.flakeModules.modules
-          home-manager.flakeModules.home-manager
-        ];
-        systems = [
-          "x86_64-linux"
-        ];
-        perSystem = {
-          config,
-          pkgs,
-          ...
-        }: {
-          # Packages and Devshells
-        };
-        flake = {
-          # NixOS modules:
-          # Home Manager modules:
-        };
-      });
+  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
