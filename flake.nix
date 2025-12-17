c1f04cd771341ae3b42f18221ecdba5c804d8d43
Jakku Night <J97k107u45k117n@gmail.com>
2025-12-16 21:13:47 -0300

    Add missing inputs + Add HomeManager input. + Add Treefmt input. + Add Disko input.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,18 @@
   inputs = {
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
     import-tree.url = "github:vic/import-tree";
     # all other inputs your flake needs, like nixpkgs.
+    home-manager = {
+      url = "github:nix-community/home-manager/";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "https://flakehub.com/f/nix-community/disko/*";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix.url = "https://flakehub.com/f/numtide/treefmt-nix/*";
   };
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
