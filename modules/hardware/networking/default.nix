0e412b311f5aaecf068c89318443ea31ce616e5d
Jakku Night <J97k107u45k117n@gmail.com>
2025-12-18 13:54:27 -0300

    Add network config + Enable NetworkManager. + Force the device to use random MAC addresses. + Set WiFi to powersave.

--- modules/hardware/networking/default.nix
+++ modules/hardware/networking/default.nix
@@ -1,5 +1,18 @@
 {inputs, ...}: {
   flake.modules.nixos."hardware/networking" = {pkgs, ...}: {
     networking.useDHCP = true;
+    networking.hostName = "nitro"; # Define your hostname.
+    # Pick only one of the below networking options.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.networkmanager = {
+      enable = true; # Easiest to use and most distros use this by default.
+      wifi = {
+        powersave = true;
+        macAddress = "random";
+      };
+      ethernet = {
+        macAddress = "random";
+      };
+    };
   };
 }
