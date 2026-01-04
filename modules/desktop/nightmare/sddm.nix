9b927ec4dfe1f9706268421f914e4cfa62890dfd
Jakku Night <J97k107u45k117n@gmail.com>
2026-01-04 11:57:39 -0300

    Fixing silentSDDM theme. + Try setting profile image and custom backgrounds.

--- modules/desktop/nightmare/sddm.nix
+++ modules/desktop/nightmare/sddm.nix
@@ -1,12 +1,29 @@
 {inputs, ...}: {
   flake.modules.nixos."desktop/nightmare/sddm" = {
+    config,
+    lib,
+    ...
+  }: {
     imports = [
       inputs.silentSDDM.nixosModules.default
     ];
     programs.silentSDDM = {
       enable = true;
       theme = "default";
-      # settings = { ... }; see example in module
+      backgrounds = {
+        "9s-wallpaper" = toString (config.stylix.image); # 9s-wallpaper.jpg
+      };
+      profileIcons = builtins.mapAttrs (username: userdata: "${userdata.home}/.face") (
+        lib.filterAttrs (username: userdata: userdata.isNormalUser || username == "root") config.users.users
+      );
+      settings = {
+        "LoginScreen" = {
+          background = "9s-wallpaper.jpg";
+        };
+        "LockScreen" = {
+          background = "9s-wallpaper.jpg";
+        };
+      };
     };
   };
 }
