df2c2efa81a758a9fa66c0b0b88816d4fce9f477
Jakku Night <J97k107u45k117n@gmail.com>
2026-01-28 19:19:51 -0300

    Config some alternative nameservers + Add Cloudflare DNS resolution.
    NOTE:
    This was removed due to conflicts with the libvirt/dnsmasq service.

--- modules/hardware/networking/default.nix
+++ modules/hardware/networking/default.nix
@@ -1,13 +1,13 @@
-{inputs, ...}: {
-  flake.modules.nixos."hardware/networking" = {
-    pkgs,
-    lib,
-    ...
-  }: {
+_: {
+  flake.modules.nixos."hardware/networking" = {lib, ...}: {
     networking.useDHCP = lib.mkDefault true;
     networking.hostName = "nitro"; # Define your hostname.
     # Pick only one of the below networking options.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # networking.nameservers = lib.mkDefault [
+    #   "1.1.1.1"
+    #   "1.0.0.1"
+    # ];
     networking.networkmanager = {
       enable = true; # Easiest to use and most distros use this by default.
       wifi = {
