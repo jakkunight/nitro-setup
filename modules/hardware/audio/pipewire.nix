db5ab294fbe1cf830d17f33cfb8dbf0499e2287f
Jakku Night <J97k107u45k117n@gmail.com>
2026-02-10 19:55:04 -0300

    Fix audio issued with bluetooth
    + Fix Wireplumber config for Bluetooth devices.
    
    Signed-off-by: Jakku Night <J97k107u45k117n@gmail.com>

--- modules/hardware/audio/pipewire.nix
+++ modules/hardware/audio/pipewire.nix
@@ -19,16 +19,6 @@
             ];
           };
         };
-        configPackages = [
-          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
-            monitor.bluez.properties = {
-              bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
-              bluez5.codecs = [ sbc sbc_xq aac ]
-              bluez5.enable-sbc-xq = true
-              bluez5.hfphsp-backend = "native"
-            }
-          '')
-        ];
         package = pkgs.wireplumber;
       };
       pulse.enable = true;
@@ -39,6 +29,7 @@
     environment.systemPackages = with pkgs; [
       pavucontrol
       helvum
+      bluez5
     ];
   };
 }
