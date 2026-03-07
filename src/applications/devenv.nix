3e3975fcef15bd936c60c9fd53864cf85ab6c4a5
Jakku Night <J97k107u45k117n@gmail.com>
2026-03-07 10:16:56 -0300

    Fix Devenv Cachix warning
    + Add Cachix substituter for Devenv.
    
    Signed-off-by: Jakku Night <J97k107u45k117n@gmail.com>

--- src/applications/devenv.nix
+++ src/applications/devenv.nix
@@ -16,6 +16,10 @@ in
           enableXonshIntegration = true;
           enableZshIntegration = true;
         };
+        nix.extraOptions = ''
+          extra-substituters = https://devenv.cachix.org
+          extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+        '';
       };
     homeManager.${feature} =
       { pkgs, ... }:
@@ -31,6 +35,10 @@ in
           enableZshIntegration = true;
           nix-direnv.enable = true;
         };
+        nix.extraOptions = ''
+          extra-substituters = https://devenv.cachix.org
+          extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+        '';
       };
   };
 }
