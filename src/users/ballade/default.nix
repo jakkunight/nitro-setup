9f0c054806b09b870cc3edc266d811fbc2016590
Jakku Night <J97k107u45k117n@gmail.com>
2026-03-15 21:16:09 -0300

    Create user 'ballade'

---
+++ src/users/ballade/default.nix
@@ -0,0 +1,72 @@
+let
+  user = "ballade";
+in
+{
+  self,
+  lib,
+  ...
+}:
+{
+  flake.modules = lib.mkMerge [
+    (self.factory.mkUser {
+      name = user;
+      uid = 1001;
+      isAdmin = true;
+      hasNetworkAccess = true;
+    })
+    (self.factory.mkHomeManagerNixosModuleConfiguration { name = user; })
+    (self.factory.mkSystemSecrets {
+      owner = user;
+      defaultSopsFile = ./secrets.yaml;
+      secrets = [
+        "ballade"
+      ];
+    })
+    {
+      nixos.${user} =
+        { pkgs, ... }:
+        {
+          imports = with self.modules.nixos; [
+            core
+            gaming
+            libvirt
+            ballade-home
+          ];
+          users.users.${user} = {
+            useDefaultShell = false;
+            shell = pkgs.zsh;
+            extraGroups = [
+              "libvirtd"
+            ];
+          };
+
+          environment.sessionVariables = {
+            EDITOR = "hx";
+          };
+
+        };
+      homeManager.${user} = {
+        imports = with self.modules.homeManager; [
+          core
+          kanagawa-theme
+          gaming
+          nightmare-desktop
+          swaync
+          libreoffice
+          nightmare-helix
+        ];
+        home = {
+          username = "${user}";
+          homeDirectory = "/home/${user}";
+          stateVersion = "26.05";
+        };
+      };
+    }
+  ];
+  flake.homeConfigurations.${user} = self.factory.mkHomeConfiguration {
+    inherit user;
+    extraModules = [
+      self.modules.homeManager.stylix-standalone-hm
+    ];
+  };
+}
