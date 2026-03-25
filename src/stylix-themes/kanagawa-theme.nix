6aaaa18f42aaf879e385e85d4e479bd998ef44b6
Jakku Night <J97k107u45k117n@gmail.com>
2026-03-25 07:44:57 -0300

    Change wallpaper.

--- src/stylix-themes/kanagawa-theme.nix
+++ src/stylix-themes/kanagawa-theme.nix
@@ -29,7 +29,7 @@ in
           polarity = "dark";
           image = "${
             self.packages.${pkgs.stdenv.hostPlatform.system}.wanderer-wallpapers
-          }/share/wallpapers/wanderer-traditional-japanese-picture.jpg";
+          }/share/wallpapers/scaramouche-sword-field.jpg";
           fonts = {
             serif = {
               package = inputs.genshin-font.packages.${pkgs.stdenv.hostPlatform.system}.default;
@@ -87,7 +87,7 @@ in
           polarity = "dark";
           image = "${
             self.packages.${pkgs.stdenv.hostPlatform.system}.wanderer-wallpapers
-          }/share/wallpapers/wanderer-traditional-japanese-picture.jpg";
+          }/share/wallpapers/scaramouche-sword-field.jpg";
           fonts = {
             serif = {
               package = inputs.genshin-font.packages.${pkgs.stdenv.hostPlatform.system}.default;
