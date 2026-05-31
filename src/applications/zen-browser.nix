27ce84c2d786bff30d2d8c72fb0594152363ff05
Jakku Night <J97k107u45k117n@gmail.com>
2026-05-31 12:04:10 -0300

    Set new default mod
    - Add unloaded tabs mod.

--- src/applications/zen-browser.nix
+++ src/applications/zen-browser.nix
@@ -5,7 +5,7 @@ in
 {
   flake.modules = {
     homeManager.${feature} =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       {
         imports = [
           inputs.zen-browser.homeModules.twilight
@@ -14,8 +14,9 @@ in
           enable = true;
           profiles."default" = {
             extensions.force = true;
-            mods = [
+            mods = lib.mkAfter [
               "642854b5-88b4-4c40-b256-e035532109df"
+              "f7c71d9a-bce2-420f-ae44-a64bd92975ab"
             ];
           };
         };
