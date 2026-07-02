47e835c2aa123a2487c040cc62dfc5c5b9540e51
Jakku Night <J97k107u45k117n@gmail.com>
2026-07-02 10:53:14 -0300

    Enable insecure package Heroic

--- src/applications/heroic.nix
+++ src/applications/heroic.nix
@@ -4,23 +4,38 @@ in
 {
   flake.modules = {
     nixos.${feature} =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       {
+        # Since Heroic uses a vulnerable version of PNPM:
+        nixpkgs.config.permittedInsecurePackages = [
+          "pnpm-10.29.2"
+        ];
         environment.systemPackages = with pkgs; [
           heroic
           mangohud
           gamemode
+          wine-wayland
+          wine64
         ];
       };
     homeManager.${feature} =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       {
         programs.mangohud = {
           enable = true;
         };
+        # Since Heroic uses a vulnerable version of PNPM:
+        nixpkgs.config.permittedInsecurePackages = [
+          "pnpm-10.29.2"
+        ];
         home.packages = with pkgs; [
           heroic
           gamemode
+          wine-wayland
+          wine64
+          _7zz
+          cabextract
+          zenity
         ];
       };
   };
