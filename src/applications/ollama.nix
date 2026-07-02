7a0ead81c18a55e60d0201b5d3b5187df9290aee
Jakku Night <J97k107u45k117n@gmail.com>
2026-07-02 10:52:29 -0300

    Config Ollama and Llama-cpp

--- src/applications/ollama.nix
+++ src/applications/ollama.nix
@@ -4,79 +4,156 @@ let
   extra-feature-2 = "ollama-vulkan";
 
 in
-{
+{ inputs, ... }: {
   flake.modules.nixos.${feature} =
     { pkgs, ... }:
     {
       environment.systemPackages = with pkgs; [
-        ollama
-        llama-cpp-vulkan
+        # ollama
+        # llama-cpp-vulkan
         lmstudio
         # vllm
         llmfit
 
       ];
+      services = {
+        ollama = {
+          enable = true;
+          environmentVariables = {
+            OLLAMA_CONTEXT_LENGTH = "262144";
+          };
+        };
+        llama-cpp = {
+          enable = true;
+          package = (pkgs.llama-cpp.override { cudaSupport = true; });
+          settings = {
+            ctx-size = 262144;
+          };
+        };
+      };
     };
   flake.modules.homeManager.${feature} =
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
-        ollama
-        llama-cpp-vulkan
+        # llama-cpp-vulkan
+        (llama-cpp.override { cudaSupport = true; })
         lmstudio
         # vllm
         llmfit
-
       ];
+      services.ollama = {
+        enable = true;
+        environmentVariables = {
+          OLLAMA_CONTEXT_LENGTH = "262144";
+          OLLAMA_NUM_PARALLEL = "1";
+          OLLAMA_FLASH_ATENTION = "1";
+          OLLAMA_MAX_LOADED_MODELS = "1";
+          OLLAMA_KV_CACHE_TYPE = "q4_0";
+          OLLAMA_SCHED_SPREAD = "0";
+          OLLAMA_MULTIUSER_CACHE = "0";
+          # OLLAMA_NEW_ENGINE = "1";
+        };
+      };
     };
   # ======== OLLAMA CUDA ========
   flake.modules.nixos.${extra-feature} =
     { pkgs, ... }:
     {
       environment.systemPackages = with pkgs; [
-        ollama-cuda
-        llama-cpp-vulkan
+        # llama-cpp-vulkan
+        (llama-cpp.override { cudaSupport = true; })
         lmstudio
         # vllm
         llmfit
 
       ];
+      services.ollama = {
+        enable = true;
+        environmentVariables = {
+          OLLAMA_CONTEXT_LENGTH = "262144";
+          OLLAMA_NUM_PARALLEL = "1";
+          OLLAMA_FLASH_ATENTION = "1";
+          OLLAMA_MAX_LOADED_MODELS = "1";
+          OLLAMA_KV_CACHE_TYPE = "q4_0";
+          OLLAMA_SCHED_SPREAD = "0";
+          OLLAMA_MULTIUSER_CACHE = "0";
+        };
+        package = pkgs.ollama-cuda;
+      };
     };
   flake.modules.homeManager.${extra-feature} =
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
-        ollama-cuda
-        llama-cpp-vulkan
+        # llama-cpp-vulkan
+        (llama-cpp.override { cudaSupport = true; })
         lmstudio
         # vllm
         llmfit
-
       ];
+      services.ollama = {
+        enable = true;
+        environmentVariables = {
+          OLLAMA_CONTEXT_LENGTH = "262144";
+          OLLAMA_NUM_PARALLEL = "1";
+          OLLAMA_FLASH_ATENTION = "1";
+          OLLAMA_MAX_LOADED_MODELS = "1";
+          OLLAMA_KV_CACHE_TYPE = "q4_0";
+          OLLAMA_SCHED_SPREAD = "0";
+          OLLAMA_MULTIUSER_CACHE = "0";
+        };
+        package = pkgs.ollama-cuda;
+      };
     };
   # ======== OLLAMA VULKAN ========
   flake.modules.nixos.${extra-feature-2} =
     { pkgs, ... }:
     {
       environment.systemPackages = with pkgs; [
-        ollama-vulkan
-        llama-cpp-vulkan
+        # llama-cpp-vulkan
+        (llama-cpp.override { cudaSupport = true; })
         lmstudio
         # vllm
         llmfit
 
       ];
+      services.ollama = {
+        enable = true;
+        environmentVariables = {
+          OLLAMA_CONTEXT_LENGTH = "262144";
+          OLLAMA_NUM_PARALLEL = "1";
+          OLLAMA_FLASH_ATENTION = "1";
+          OLLAMA_MAX_LOADED_MODELS = "1";
+          OLLAMA_KV_CACHE_TYPE = "q4_k_m";
+          OLLAMA_SCHED_SPREAD = "0";
+          OLLAMA_MULTIUSER_CACHE = "0";
+        };
+        package = pkgs.ollama-vulkan;
+      };
     };
   flake.modules.homeManager.${extra-feature-2} =
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
-        ollama-vulkan
-        llama-cpp-vulkan
+        # llama-cpp-vulkan
+        (llama-cpp.override { cudaSupport = true; })
         lmstudio
         # vllm
         llmfit
-
       ];
+      services.ollama = {
+        enable = true;
+        environmentVariables = {
+          OLLAMA_CONTEXT_LENGTH = "262144";
+          OLLAMA_NUM_PARALLEL = "1";
+          OLLAMA_FLASH_ATENTION = "1";
+          OLLAMA_MAX_LOADED_MODELS = "1";
+          OLLAMA_KV_CACHE_TYPE = "q4_k_m";
+          OLLAMA_SCHED_SPREAD = "0";
+          OLLAMA_MULTIUSER_CACHE = "0";
+        };
+        package = pkgs.ollama-vulkan;
+      };
     };
 }
