1221fe5d988c743c340c3f046924ddc89e175c2e
Jakku Night <J97k107u45k117n@gmail.com>
2026-07-17 07:04:59 -0300

    Add new package.
    - Add Jakku wallpapers.
    
    Signed-off-by: Jakku Night <J97k107u45k117n@gmail.com>

---
+++ src/packages/lohen-wallpapers/default.nix
@@ -0,0 +1,19 @@
+let
+  package = "lohen-wallpapers";
+in
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.${package} = pkgs.stdenvNoCC.mkDerivation {
+        name = "${package}";
+        src = ./.;
+        dontBuild = true;
+        dontUnpack = true;
+        installPhase = ''
+          mkdir -p $out/share
+          cp -R $src/* $out/share
+        '';
+      };
+    };
+}
